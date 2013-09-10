09d1bbaaa9b0f7eb365d4f1fe2aa0c34ea4ed22b
A.B. Carroll <ben@hl9.net>
2013-09-09 23:18:57 -0500

    Added the (untested) fixed delay throttler.
    This throttler is the most basic throttler.  The code is untested.

---
+++ lib/core/fixed_delay_throttler.php
@@ -0,0 +1,16 @@
+<?php
+ /*
+  * This file is a part of Hubbub, freely available at http://hubbub.sf.net
+  *
+  * Copyright (c) 2013, Armond B. Carroll <ben@hl9.net>
+  * For full license terms, please view the LICENSE.txt file that was
+  * distributed with this source code.
+  */
+
+	class fixed_delay_throttler extends base_throttler {
+		function throttle() {
+			parent::throttle();
+			trigger_error("[{$this->iteration}] Sleeping for {$this->frequency}");
+			usleep($this->frequency);
+		}
+	}
