3bab1432e6b43b04464bd136dc1a74aeb96fb830
A.B. Carroll <ben@hl9.net>
2015-01-18 23:16:07 -0500

    phpDoc updated for net_generic_client

--- lib/core/net_generic_client.php
+++ lib/core/net_generic_client.php
@@ -8,16 +8,45 @@
  * distributed with this source code.
  */
 
+/**
+ * Interface net_generic_client
+ */
 interface net_generic_client extends net_generic {
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
     function send($data);
 
+    /**
+     * @param $length
+     *
+     * @return mixed
+     */
     function recv($length);
 
+    /**
+     * @return mixed
+     */
     function on_connect();
 
+    /**
+     * @return mixed
+     */
     function on_disconnect();
 
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
     function on_send($data);
 
+    /**
+     * @param $data
+     *
+     * @return mixed
+     */
     function on_recv($data);
 }
