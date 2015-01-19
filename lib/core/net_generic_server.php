eb01c80724f8f636eec07b6eb948e6ee1e059c4f
A.B. Carroll <ben@hl9.net>
2015-01-18 22:58:43 -0500

    phpDoc updated for net_generic_server

--- lib/core/net_generic_server.php
+++ lib/core/net_generic_server.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is a part of Hubbub, freely available at http://hubbub.sf.net
  *
@@ -8,16 +7,53 @@
  * distributed with this source code.
  */
 
+/**
+ * Interface net_generic_server
+ */
 interface net_generic_server extends net_generic {
+    /**
+     * @param $socket
+     * @param $data
+     *
+     * @return mixed
+     */
     function send($socket, $data);
 
+    /**
+     * @param $socket resource
+     * @param $length int
+     *
+     * @return mixed
+     */
     function recv($socket, $length);
 
+    /**
+     * @param $socket resource The newly connected socket resource
+     *
+     * @return void
+     */
     function on_client_connect($socket);
 
+    /**
+     * @param $socket resource The recently disconnected socket resource
+     *
+     * @return void
+     */
     function on_client_disconnect($socket);
 
+    /**
+     * @param $socket resource The socket that is sending data
+     * @param $data string The data that is being sent
+     *
+     * @return void
+     */
     function on_client_send($socket, $data);
 
+    /**
+     * @param $socket resource The client that received the data.
+     * @param $data string The data received. This data may be binary or ASCII text.
+     *
+     * @return mixed
+     */
     function on_client_recv($socket, $data);
 }
