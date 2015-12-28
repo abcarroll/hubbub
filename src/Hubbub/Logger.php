1f15ebcbd9fb15b4ce8536fcf2e766daaa607afc
A.B. Carroll <ben@hl9.net>
2015-12-28 02:18:32 -0500

    "context" substitution and other improvements

--- src/Hubbub/Logger.php
+++ src/Hubbub/Logger.php
@@ -12,21 +12,14 @@
 
 namespace Hubbub;
 
-    /**
-     * A logger interface that "looks like" the PSR-3 interface.
-     * Note it does not technically, but practically, implements PsrLogLoggerInterface.
-     * See https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md
-     *
-     * @todo Actual PSR-3 usage.
-     * @todo phpDoc Updates
-     */
-
 /**
- * Class Logger
+ * A simple logger that implements the PSR-3 LoggerInterface and logs internal Hubbub messages to file.
  *
  * @package Hubbub
+ * @see     https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md
+ * @see     https://bitbucket.org/leverton/mefworks-log.git A decent implementation of filtering
  */
-class Logger { // extends PsrLogAbstractLogger implements PsrLogLoggerInterface {
+class Logger extends \Psr\Log\AbstractLogger implements \Psr\Log\LoggerInterface {
     /**
      * @var Logger        $instance
      * @var Configuration $conf
@@ -34,6 +27,11 @@ class Logger { // extends PsrLogAbstractLogger implements PsrLogLoggerInterface 
      */
     protected $conf, $fp;
 
+    /**
+     * Logger constructor.
+     *
+     * @param Configuration|null $conf
+     */
     public function __construct(\Hubbub\Configuration $conf = null) {
         $this->conf = $conf;
         if(!empty($this->conf->get('logger.logToFile'))) {
@@ -49,7 +47,25 @@ class Logger { // extends PsrLogAbstractLogger implements PsrLogLoggerInterface 
         }
     }
 
+    /**
+     * Log with an arbitrary level.
+     *
+     * Currently, the only 'feature' is to log all output to both the 'logger.logToFile' configuration value as well as stdout.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return null
+     *
+     * @todo Implement filtering and obvious features.
+     */
     public function log($level, $message, array $context = array()) {
+        // Perform PSR-3 substitution
+        foreach($context as $cKey => $cVal) {
+            $message = str_replace("{$cKey}", $cVal, $message);
+        }
+
         // Generate a pretty message
         $prefixStr = "[" . date('h:i:sA') . "] [$level] ";
         $logText = $prefixStr . str_replace("\n", "\n$prefixStr", $message);
@@ -59,67 +75,31 @@ class Logger { // extends PsrLogAbstractLogger implements PsrLogLoggerInterface 
             fwrite($this->fp, $logText . "\n");
         }
 
-        // Print a "context dump" (think core dump) on severe errors
-        switch($level) {
-            case 'emergency':
-            case 'alert':
-            case 'critical':
-            case 'error':
-            case 'warning':
-                // TODO Make configurable.  Off until then, this is really meant more for long running instances.
-                // $this->dumpContext($logText, $context);
-            break;
+        if(isset($context['state-dump']) && $context['state-dump'] == true) {
+            $this->dumpState($message, $context);
         }
     }
 
-    public function emergency($message, array $context = array()) {
-        $this->log('emergency', $message, $context);
-    }
-
-    public function alert($message, array $context = array()) {
-        $this->log('alert', $message, $context);
-    }
-
-    public function critical($message, array $context = array()) {
-        $this->log('critical', $message, $context);
-    }
-
-    public function error($message, array $context = array()) {
-        $this->log('error', $message, $context);
-    }
-
-    public function warning($message, array $context = array()) {
-        $this->log('warning', $message, $context);
-    }
-
-    public function notice($message, array $context = array()) {
-        $this->log('notice', $message, $context);
-    }
-
-    public function info($message, array $context = array()) {
-        $this->log('info', $message, $context);
-    }
-
-    public function debug($message, array $context = array()) {
-        $this->log('debug', $message, $context);
-    }
-
     /**
-     * Generates and writes a "context dump" to the local working directory.
+     * Generates and writes a "state dump" to the local working directory.
+     *
+     * Think core dump.
      *
      * @param       $logText
      * @param array $context
      */
-    protected function dumpContext($logText, array $context) {
-        $logText = date('r') . "\n\n" . $logText;
+    protected function dumpState($logText, array $context) {
         ob_start();
         var_dump($context);
+        var_dump($GLOBALS);
+
+        $logText = date('r') . "\n\nMessage: " . $logText;
         $logText .= "\n\nContext Dump As Follows:\n\n";
         $logText .= ob_get_clean();
 
         $fileName = 'context-' . sha1($logText) . '.txt';
 
-        if(@file_put_contents($fileName, $logText)) {
+        if(@file_put_contents(Utility::baseDir() . DIRECTORY_SEPARATOR . $fileName, $logText)) {
             echo "\n* * * Context dump written to file: $fileName *\n\n";
         } else {
             fwrite(STDERR, "\n** ** ** Could not write context dump to file: $fileName *\n\n");
