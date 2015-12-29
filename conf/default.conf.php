f72d4d34da1ba37d1a9f5a74865930b49ba4904d
A.B. Carroll <ben@hl9.net>
2015-12-29 14:17:54 -0500

    implemented DNS options, DNS cache, DNS timeouts, better error handling

--- conf/default.conf.php
+++ conf/default.conf.php
@@ -5,96 +5,110 @@
 
 return [
     // These are the objects to auto-create
-    'hubbub'             => [
-        'dns'      => '\Hubbub\DNS\DigResolver',
-        'bnc'      => '\Hubbub\IRC\Bnc',
+    'hubbub'    => [
+        'dns'       => '\Hubbub\DNS\DigResolver',
+        'bnc'       => '\Hubbub\IRC\Bnc',
         //'example'  => '\Hubbub\ExampleProtocol\Client',
-        'freenode' => '\Hubbub\IRC\Client',
+        'localhost' => '\Hubbub\IRC\Client',
     ],
 
     // This actually works
-    'logger'             => [
+    'logger'    => [
         'logToFile' => 'log/console.log',
     ],
 
     // This actually works
-    'throttler'          => [
+    'throttler' => [
         'frequency' => 100000,
+    ],
+
+    'dns' => [
+        // This should work on most systems.  If not, try '/usr/bin/dig'
+        'dig-path' => 'dig',
+
+        // actual timeout will be (timeout) * (tries)
+        'default-timeout' => 3,
+        'default-tries' => 4,
+
+        // TODO: Only the first DNS server in this set is used
+        'servers'     => [
+            '8.8.8.8',
+            '8.8.4.4'
+        ],
     ],
 
     // Our local "test" configuration; this should be removed once we are beyond a certain point of usability
     'irc'                => [
-        'freenode' => [
-            // Peg the server list to a dynamic server list.
-            'serverListPegged' => 'freenode',
-            // The list of servers, or if it is pegged, the cached/last updated copy of the server list
-            'serverList'       => [
-                'irc.freenode.net:6667',
-            ],
-            'modules'          => [
-
-                /*
-                 * What is the best way to de-couple, or possibly rather properly couple, things such as channel management (keep topic, keep modules,
-                 * services integration, etc) along with more simpler aspects such as auto-join, etc?
-                 *
-                 * Or more confusingly, say we want to log only #a and #b but not #c, -- this creates friction with the Logger.
-                 */
-
-                'channels' => [
-                    '#hubbub'
-                ]
-            ]
-
+    'localhost' => [
+        // Peg the server list to a dynamic server list. (Doesn't work)
+        // 'serverListPegged' => 'freenode',
+        // The list of servers, or if it is pegged, the cached/last updated copy of the server list
+        'serverList' => [
+            '127.0.0.1:6667',
         ],
+        'modules'    => [
+            /*
+             * What is the best way to de-couple, or possibly rather properly couple, things such as channel management (keep topic, keep modules,
+             * services integration, etc) along with more simpler aspects such as auto-join, etc?
+             *
+             * Or more confusingly, say we want to log only #a and #b but not #c, -- this creates friction with the Logger.
+             */
 
-        'bnc' => [
-            'listen'    => '0.0.0.0:1337',
-            'password'  => '1234',
-            'motd_file' => 'LICENSE.txt',
+            'channels' => [
+                '#hubbub'
+            ]
         ]
+
     ],
+
+    'bnc' => [
+        'listen'    => '0.0.0.0:1338',
+        'password'  => '1234',
+        'motd_file' => 'LICENSE.txt',
+    ]
+],
 
     // This is meant to be the default configuration for this class, but it doesn't work yet
     '\Hubbub\IRC\Client' => [
-        'nickname'  => 'HubTest-' . mt_rand(0, 999),
-        'username'  => 'Hubbub',
-        'realname'  => 'Hubbub',
+    'nickname'  => 'HubTest-' . mt_rand(0, 999),
+    'username'  => 'Hubbub',
+    'realname'  => 'Hubbub',
 
-        // Default part & quit messages if none are specified otherwise
-        'partmsg'   => '',
-        'quitmsg'   => 'I use Hubbub, the php irc client: http://github.com/nezzario/hubbub',
+    // Default part & quit messages if none are specified otherwise
+    'partmsg'   => '',
+    'quitmsg'   => 'I use Hubbub, the php irc client: http://github.com/nezzario/hubbub',
 
-        // How long to wait between reconnects, or 0 to never reconnect if disconnected
-        'reconnect' => 30,
-        'modules'   => [
+    // How long to wait between reconnects, or 0 to never reconnect if disconnected
+    'reconnect' => 30,
+    'modules'   => [
 
-            /*
-             * This is interesting as we need to load these modules for /each/ irc object
-             * In addition, we need to be able to not "copy" these configurations, so e.x.
-             * you should be able to update the keepNick.retry variable for the global
-             * setting, and if I had keepNick.retry = 0 (off) for network A, then it would
-             * not affect network A's setting but only if it was inherited
-             */
+        /*
+         * This is interesting as we need to load these modules for /each/ irc object
+         * In addition, we need to be able to not "copy" these configurations, so e.x.
+         * you should be able to update the keepNick.retry variable for the global
+         * setting, and if I had keepNick.retry = 0 (off) for network A, then it would
+         * not affect network A's setting but only if it was inherited
+         */
 
-            'keepNick' => [
-                // How long to wait between nick regain retries
-                'retry' => 300,
-            ],
-            /*
-             * These are more complicated as they seem because they require interaction with a BNC, and as such their
-             * configuration is stuck somewhere in-between the IRC and BNC realm.
-             */
+        'keepNick' => [
+            // How long to wait between nick regain retries
+            'retry' => 300,
+        ],
+        /*
+         * These are more complicated as they seem because they require interaction with a BNC, and as such their
+         * configuration is stuck somewhere in-between the IRC and BNC realm.
+         */
 
-            'ctcp' => [
-                'mode'    => 'static', // Needs a static mode, pass-thru mode, block mode, pass-thru-when-active (ie pass thru if bnc is active)
-                'version' => 'I use Hubbub, the php irc client: http://github.com/nezzario/hubbub'
-            ],
-            'dcc'  => [
-                'file' => [
-                    'file' => 'auto-accept|prompt|ignore|pass-thru',
-                    'chat' => 'auto-accept|prompt|ignore|pass-thru',
-                ]
+        'ctcp' => [
+            'mode'    => 'static', // Needs a static mode, pass-thru mode, block mode, pass-thru-when-active (ie pass thru if bnc is active)
+            'version' => 'I use Hubbub, the php irc client: http://github.com/nezzario/hubbub'
+        ],
+        'dcc'  => [
+            'file' => [
+                'file' => 'auto-accept|prompt|ignore|pass-thru',
+                'chat' => 'auto-accept|prompt|ignore|pass-thru',
             ]
         ]
-    ],
+    ]
+],
 ];
--- src/Hubbub/DNS/DigResolver.php
+++ src/Hubbub/DNS/DigResolver.php
@@ -13,7 +13,7 @@
 namespace Hubbub\DNS;
 
 /**
- * Class ShellResolver
+ * Class DigResolver
  * @package Hubbub\DNS
  */
 class DigResolver implements \Hubbub\Protocol\Client, \Hubbub\Iterable {
@@ -23,56 +23,183 @@ class DigResolver implements \Hubbub\Protocol\Client, \Hubbub\Iterable {
     protected $logger;
 
     /**
+     * @var \Hubbub\Configuration
+     */
+    protected $conf;
+
+    /**
      * @var \Hubbub\MessageBus
      */
     protected $bus;
 
     /**
+     * The process manager object
      * @var \Hubbub\ProcessManager
      */
     protected $processManager;
 
-    protected $idHostTable = [];
+    /**
+     * A table containing every "id" with the host it is resolving and when resolution began
+     * @var array
+     */
+    protected $idTable = [];
 
-    public function __construct(\Hubbub\ProcessManager $processManager, \Hubbub\Logger $logger, \Hubbub\MessageBus $bus, $name) {
+    /**
+     * Contains recently cached lookups
+     * @var array
+     */
+    protected $resolverCache = [];
+
+    /**
+     * Last garbage collection as a unix timestamp
+     * @var int
+     */
+    protected $lastGc = 0;
+
+    /**
+     * How long in between cache invalidation
+     * @var int
+     */
+    protected $gcEverySec = 30;
+
+    public function __construct(\Hubbub\ProcessManager $processManager, \Hubbub\Logger $logger, \Hubbub\Configuration $conf, \Hubbub\MessageBus $bus, $name) {
         $this->processManager = $processManager;
         $this->logger = $logger;
+        $this->conf = $conf;
         $this->bus = $bus;
 
         $this->bus->subscribe([$this, 'handleBusMessage']);
     }
 
-    public function handleBusMessage($msg) {
-        if(isset($msg['action']) && $msg['action'] == 'resolve') {
-            $id = $this->processManager->execute('dig +short ' . escapeshellarg($msg['host']));
-            $this->idHostTable[$id] = $msg['host'];
+    public function handleResponse($msg, $cacheResponse = true) {
+        $this->bus->publish($msg);
+        if($cacheResponse && $msg['status'] == 'ok') {
+            $msg['expires-at'] = time() + 3600;
+        } else {
+            // Only cache negative responses for 30 sec, this is more-or-less to prevent creating a flood of dig processes if something keeps requesting it
+            $msg['expires-at'] = time() + 30;
         }
+
+        $this->resolverCache[$msg['host']] = $msg;
+
+    }
+
+
+    /**
+     * Handles DNS protocol bus messages
+     *
+     * @param $msg
+     *
+     * @throws \ErrorException
+     *
+     * @todo Implement bus filters and remove if() statements regarding protocol/action selection
+     */
+    public function handleBusMessage($msg) {
+        if(!(isset($msg['protocol']) && $msg['protocol'] == 'dns')) {
+            return $msg;
+        }
+
+        // Handle action=resolve
+        if(isset($msg['action']) && $msg['action'] == 'resolve') {
+            // Is it already cached?
+            if(isset($this->resolverCache[$msg['host']]) && $this->resolverCache[$msg['host']]['expires-at'] < time()) {
+                $this->handleResponse($this->resolverCache[$msg['host']], false);
+                return $msg;
+            }
+
+            $timeout = isset($msg['timeout']) ? $msg['timeout'] : $this->conf->get('dns.default-timeout');
+            $timeout = empty($timeout) ? 3 : $timeout;
+
+            $tries = isset($msg['tries']) ? $msg['tries'] : $this->conf->get('dns.default-tries');
+            $tries = empty($tries) ? 4 : $tries;
+
+            $commandLine = escapeshellcmd($this->conf->get('dns.dig-path')) .
+                           ' @' . $this->conf->get('dns.servers.0') .
+                           ' ' . escapeshellarg($msg['host']) .
+                           ' +tries=' . escapeshellarg($tries) .
+                           ' +timeout=' . escapeshellarg($timeout) .
+                           ' +short';
+
+            try {
+                $id = $this->processManager->execute($commandLine);
+
+                $this->idTable[$id] = [
+                    'host'       => $msg['host'],
+                    'timeout-at' => time() + $timeout
+                ];
+            } catch(\ErrorException $e) {
+                $this->logger->critical("Failed to start 'dig': " . $e->getMessage());
+
+                $this->handleResponse([
+                    'protocol' => 'dns',
+                    'action'   => 'response',
+                    'status'   => 'error',
+                    'host'     => $msg['host'],
+                    'message'  => 'Failed to start dig for DNS resolution',
+                ]);
+            }
+        } elseif(isset($msg['action']) && $msg['action'] == 'flush-cache') {
+            $this->resolveCache = [];
+        }
+
+        return $msg;
     }
 
     public function iterate() {
         $completed = $this->processManager->pollCompleted();
 
+        // Handle completions
         if(count($completed) > 0) {
-            foreach($completed as $id => $result) {
-                $host = $this->idHostTable[$id];
-                // TODO: the right thing, this is a quick fix
-                $result2 = '';
-                $p = explode("\n", $result);
-                foreach($p as $pp) {
-                    if(filter_var($pp, FILTER_VALIDATE_IP)) {
-                        $result2 = trim($pp);
+            foreach($completed as $id => $rawOutput) {
+                $host = $this->idTable[$id]['host'];
+                $outputLines = explode("\n", $rawOutput);
+
+                $response = [];
+                foreach($outputLines as $line) {
+                    if(filter_var($line, FILTER_VALIDATE_IP)) {
+                        $response[] = trim($line);
                         break;
                     }
                 }
 
-                $this->bus->publish([
+                $this->handleResponse([
                     'protocol' => 'dns',
-                    'action'   => 'resolve-complete',
+                    'action'   => 'response',
+                    'status'   => 'complete',
                     'host'     => $host,
-                    'result'   => $result2,
+                    'result'   => $response,
                 ]);
+
+                unset($this->idTable[$id]);
             }
         }
+
+        // Garbage collection: timeouts
+
+        foreach($this->idTable as $id => $m) {
+            // This should never happen since dig should always gracefully timeout itself, which is why we give an additional 60 seconds grace period
+            if(time() > ($m['timeout-at'] + 10)) {
+                $this->handleResponse([
+                    'protocol' => 'dns',
+                    'action'   => 'response',
+                    'status'   => 'timeout',
+                    'host'     => $m['host'],
+                    'message'  => 'The DNS "dig" never responded.'
+                ]);
+                unset($this->idTable[$id]);
+            }
+        }
+
+        if(time() > ($this->lastGc + 30)) {
+            // Garbage collection: cache
+            foreach($this->resolverCache as $host => $c) {
+                if(time() > $c['expires-at']) {
+                    unset($this->resolverCache[$host]);
+                }
+            }
+            $this->lastGc = time();
+        }
+
     }
 
     /*
